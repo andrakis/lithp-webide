48ce99ecb9bcdd56a90edc9379dc99433cda0be7
Julian Thatcher <daedalus.raistlin@gmail.com>
2017-02-09 14:30:13 +1100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,27 @@
+LIBS=
+RUN=node_modules/lithp/run
+GENFILES=./genfiles.sh
+RUNFLAGS=
+EXTRA_PATHS=../../webide
+
+default: links webide lithp-pkg.js
+all: default
+
+links:
+	if [ ! -e "run" ]; then \
+		ln -s node_modules/lithp/run.js run; \
+	fi
+
+lithp-pkg.js: webide
+	$(MAKE) -C node_modules/lithp-pkg EXTRA_PATHS=$(EXTRA_PATHS)
+	cp node_modules/lithp-pkg/index.js .
+	cp node_modules/lithp-pkg/files.js .
+
+webide:
+	$(MAKE) -C webide
+
+clean:
+	rm -f lithp-pkg.js
+	rm -f index.js
+	$(MAKE) -C node_modules/lithp-pkg clean
+	$(MAKE) -C webide clean
---
+++ html/lithp-editor.js
@@ -0,0 +1,51 @@
+(function() {
+	var oldConsole = console;
+	window.fakeConsole = {
+		stdout: "",
+		stderr: "",
+		logStdout: function() {
+			fakeConsole.stdout += Array.prototype.join.call(arguments, ' ');
+			if(fakeConsole.onStdout) {
+				fakeConsole.onStdout(fakeConsole.stdout);
+				fakeConsole.stdout = "";
+			}
+		},
+		logStderr: function() {
+			fakeConsole.stderr += Array.prototype.join.call(arguments, ' ');
+			if(fakeConsole.onStderr) {
+				fakeConsole.onStderr(fakeConsole.stderr);
+				fakeConsole.stderr = "";
+			}
+		},
+		onStdout: null,
+		onStderr: null,
+	};
+	window.console = {
+		log: function() {  oldConsole.log.apply(oldConsole , arguments);
+						  fakeConsole.logStdout.apply(fakeConsole, arguments); },
+		error: function() { //oldConsole.error.apply(oldConsole , arguments);
+							fakeConsole.logStderr.apply(fakeConsole, arguments); },
+	};
+
+	var instance = new Lithp.Lithp();
+
+	window.runCode = function(code) {
+		var debugMode = document.getElementById("debugMode").checked;
+		var parserDebug=document.getElementById("parserDebugMode").checked;
+		var clearOnRun= document.getElementById("clearOnRun").checked;
+		if(clearOnRun)
+			clearConsole();
+		Lithp.set_debug_flag(debugMode);
+		global._lithp.set_parser_debug(parserDebug);
+		var result = Lithp.Parser(code, {finalize: true, ast: false});
+		instance.setupDefinitions(result, "webide");
+		// Required on packaged Lithp
+		instance.Define(result, "__AST__", Lithp.Types.Atom("true"))
+		try {
+			var timed = timeCall("Run code", () => instance.run(result));
+			console.log("Code run in " + timed[1] + "ms, result: " + timed[0]);
+		} catch (e) {
+			console.log(e.stack);
+		}
+	}
+})();
